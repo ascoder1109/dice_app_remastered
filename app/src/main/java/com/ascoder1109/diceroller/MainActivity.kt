aad41927166c03b3ba696f08d561c3a7cf4bec8a
Aditya Singh <as1671109@gmail.com>
2024-04-12 18:29:46 +0530

    Initial Commit

---
+++ app/src/main/java/com/ascoder1109/diceroller/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.ascoder1109.diceroller
+
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ascoder1109.diceroller.R.*
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.custom_green)
+        }
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+
+        // Find views
+        diceImage = findViewById(R.id.dice_image)
+
+        // Set click listener for roll button
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        // Set the image resource for the dice ImageView
+        diceImage.setImageResource(drawableResource)
+    }
+}
